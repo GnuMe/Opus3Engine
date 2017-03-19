d81a4927ead5472d9213970d2642e5139864c4bc
Ivan Stanton <2112871@jeffcoschools.us>
2017-03-18 22:47:10 -0600

    Create o3gameinit.py

---
+++ o3gameinit.py
@@ -0,0 +1,34 @@
+global roomx
+global roomy
+global roomxmax
+global roomxmin
+global roomymax
+global roomymin
+global char_hp
+global char_atk_divisor
+global char_atk_max
+global char_atk_min
+global char_def_divisor
+global char_def_max
+global world
+global koboldDefeated #game specific
+global gotKey #game specific
+global gameDone
+gameDone=False
+roomx=0
+roomy=0
+roomxmax=2
+roomxmin=-2
+roomymax=roomxmax
+roomymin=roomxmin
+char_hp=20
+char_atk_min=1
+char_atk_max=5
+char_atk_divisor=1
+char_def_min=0
+char_def_max=2
+char_def_divisor=2
+world="Opus 3 Demo"
+#game specific variables
+koboldDefeated=False
+gotKey=False
